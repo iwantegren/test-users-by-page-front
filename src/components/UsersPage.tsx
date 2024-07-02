922108d31760fac14be09458283cd8be9da44f82
Ivan Bilan <iwantegren@gmail.com>
2024-07-02 18:49:38 +0300

    add dummy user cards

--- src/components/UsersPage.tsx
+++ src/components/UsersPage.tsx
@@ -1,8 +1,63 @@
+import { useState } from "react";
+import { UserComponent } from "./User";
+
 function UsersPageComponent() {
+  const [prevPage, setPrevPage] = useState<string | null>(null);
+  const [nextPage, setNextPage] = useState<string | null>("url");
+
+  const users = [
+    { id: 1, name: "Ivan" },
+    { id: 2, name: "Eva" },
+    { id: 3, name: "Ira" },
+    { id: 4, name: "Igor" },
+  ];
+
+  const clickPrevPage = (_e: React.MouseEvent<HTMLButtonElement>) => {
+    setNextPage("url");
+    setPrevPage(null);
+  };
+
+  const clickNextPage = (_e: React.MouseEvent<HTMLButtonElement>) => {
+    setPrevPage("url");
+    setNextPage(null);
+  };
+
   return (
     <div className="card">
       <div className="card-header bg-secondary bg-gradient">List ot users</div>
-      <div className="card-body">Users...</div>
+      <div className="card-body">
+        <button
+          className="btn btn-primary mx-3"
+          disabled={prevPage === null}
+          onClick={clickPrevPage}
+        >
+          Prev
+        </button>
+        <button
+          className="btn btn-primary mx-3"
+          disabled={nextPage === null}
+          onClick={clickNextPage}
+        >
+          Next
+        </button>
+        {users.map((user) => (
+          <UserComponent key={user.id} user={user} />
+        ))}
+        <button
+          className="btn btn-primary mx-3"
+          disabled={prevPage === null}
+          onClick={clickPrevPage}
+        >
+          Prev
+        </button>
+        <button
+          className="btn btn-primary mx-3"
+          disabled={nextPage === null}
+          onClick={clickNextPage}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
